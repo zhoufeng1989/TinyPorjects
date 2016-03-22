17fbe733214eb3b914bbc375782c0b4525e32728
zhoufeng <zhoufengloop@gmail.com>
2016-03-22 22:00:37 +0800

    Update lexer.

--- lambda-lang/src/main/scala/lambda/Lexer.scala
+++ lambda-lang/src/main/scala/lambda/Lexer.scala
@@ -4,11 +4,36 @@ package lambda
  * Created by zhoufeng on 16/3/21.
  */
 class Lexer(val stream: InputStream) {
-  val keywords = List("if", "then", "else", "labmda", "λ", "true", "false")
-  val punctuation = List(",", ";", "(", ")", "{", "}", "[", "]")
+  private val keywords = List("if", "then", "else", "lambda", "λ", "true", "false")
+  private val punctuation = List(',', ';', '(', ')', '{', '}', '[', ']')
+  private val whitespaces = List(' ', '\n', '\t')
+  private var current:String = _
 
+  private def isWhitespace(char: Char): Boolean = whitespaces contains char
+  private def isNumber(char: Char): Boolean = ('0' <= char) && (char <= '9')
+
+  private def skip_comment():Unit = stream.next match {
+    case "\n" => _
+    case _ => skip_comment()
+  }
+
+  private def read_while(predicate: Char => Boolean): String = stream.peek() match {
+    case char if predicate(char) => {stream.next(); char::readWhile(predicate)}
+  }
+
+  private def read_number(): Token = {
+    stream.peek() match {
+      case char if isNumber(char) =>
+    }
+  }
+
+
+  def read_next(): Token = stream.peek match {
+    case char if isWhitespace(char) => {stream.next(); read_next()}
+    case '#' => {skip_comment(); read_next()}
+    case char if isNumber(char) => read_number()
+  }
 }
-
 abstract class Token {
   val value: Any
 
@@ -36,5 +61,3 @@ case class OpToken(value: String) extends Token
 case object EofToken extends Token {
   val value = ""
 }
-
-
