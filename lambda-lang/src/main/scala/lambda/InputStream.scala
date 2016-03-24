be065a61e2036c0eb1ef6ffaef52f535ffb712d3
zhoufeng <zhoufengloop@gmail.com>
2016-03-24 20:45:38 +0800

    Update.

--- lambda-lang/src/main/scala/lambda/InputStream.scala
+++ lambda-lang/src/main/scala/lambda/InputStream.scala
@@ -2,38 +2,34 @@ package lambda
 
 import scala.collection.mutable
 import scala.io.Source
-import scala.util.{Failure, Try, Success}
 
 /**
  * Created by zhoufeng on 16/3/21.
  */
 class InputStream(val input: Source) {
-  import InputStream.eof
-  var lineNo = 1
-  var column = 0
-  val queue = mutable.Queue[Char]()
+  private var lineNo = 1
+  private var column = 0
+  private val queue = mutable.Queue[Char]()
 
-  def next(): Try[Char] = {
-    val nextChar = if(queue.isEmpty) Try(input.next()) else Success(queue.dequeue())
+  def next(): Option[Char] = {
+    val nextChar = if(queue.isEmpty) {
+      if(input.isEmpty) None else Some(input.next())
+    } else Some(queue.dequeue())
+
     nextChar match {
-      case Success("\n") => {lineNo += 1; column = 0; Success('\n')}
-      case Success(char) => {column += 1; Success(char)}
-      case failure: Failure => failure
+      case Some("\n") => {lineNo += 1; column = 0}
+      case Some(char) => column += 1
+      case _ => _
     }
-  }
-
-  def peek(): Char = {
-    val nextChar = input.next()
-    queue.enqueue(nextChar)
     nextChar
   }
 
-  def isEof: Boolean = input.isEmpty
+  def peek(): Option[Char] = {
+    if(queue.isEmpty) {
+      if(input.isEmpty) None else {queue.enqueue(input.next()); Some(queue.head)}
+    } else Some(queue.head)
+  }
 
   def croak(message: String) = throw new Exception(s"${message} (${lineNo}: ${column})")
 
-}
-
-object InputStream {
-  case object eof
 }
