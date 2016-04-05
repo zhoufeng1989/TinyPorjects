cdd12e3e86e918976b97bcaf64497fc6f6d2eefd
zhoufeng <zhoufengloop@gmail.com>
2016-04-05 21:48:45 +0800

    Update AKManager.

--- AKManager/src/main/scala/AKManager.scala
+++ AKManager/src/main/scala/AKManager.scala
@@ -2,7 +2,10 @@
  * Created by zhoufeng on 16/4/4.
  */
 
-case class Record(alias: String, id: String, secretKey: String, region: String="us-east-1")
+case class Record(alias: String, id: String, secretKey: String, region: String="us-east-1") {
+  override def toString =
+    List(s"alias=${alias}", s"id=${id}", s"secretKey=${secretKey}", s"region=${region}").mkString("\n")
+}
 
 class AKManager(val list: List[Record]) {
   def add(record: Record): AKManager = ???
@@ -12,8 +15,10 @@ class AKManager(val list: List[Record]) {
 class Config(config_file: String) {
   def loadFromFile(): List[Record] = ???
   def saveToFile() = ???
+  def records = loadFromFile()
 }
 
 object AKManager extends App {
-
+  val config = new Config(System.getProperty("os.home") + "/" + ".akm.cfg")
+  val akManager = new AKManager(config.records)
 }
